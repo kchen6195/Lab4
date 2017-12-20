c1266ecb9892812b72cdf87e1a00ced5f1b3b824
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-12-20 13:02:23 -0500

    finished csv reader

--- Lab4/src/lab4_2/Runner.java
+++ Lab4/src/lab4_2/Runner.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Runner {
 
 	public static void main(String[] args) 
@@ -50,83 +51,10 @@ public class Runner {
 		int avgEnglish = Integer.parseInt(metadata[3]);
 		int avgMath = Integer.parseInt(metadata[4]);
 		int avgWriting = Integer.parseInt(metadata[5]);
-		return Sat(DBN,school,satTakers,avgEnglish,avgMath,avgWriting);
+		Sat test = new Sat(DBN,school,satTakers,avgEnglish,avgMath,avgWriting);
+		return test;
 	}
-	//
-	class Sat
-	{
-		private String DBN;
-		private String school;
-		private int satTakers;
-		private int avgEnglish;
-		private int avgMath;
-		private int avgWriting;
-		
-		public Sat(String DBN, String school,int satTakers, int avgEnglish,int avgMath, int avgWriting)
-		{
-			this.DBN = DBN;
-			this.school = school;
-			this.satTakers = satTakers;
-			this.avgEnglish = avgEnglish;
-			this.avgMath = avgMath;
-			this.avgWriting = avgWriting;
-			
-		}
-
-		public String getDBN() {
-			return DBN;
-		}
-
-		public void setDBN(String dBN) {
-			DBN = dBN;
-		}
-
-		public String getSchool() {
-			return school;
-		}
-
-		public void setSchool(String school) {
-			this.school = school;
-		}
-
-		public int getSatTakers() {
-			return satTakers;
-		}
-
-		public void setSatTakers(int satTakers) {
-			this.satTakers = satTakers;
-		}
-
-		public int getAvgEnglish() {
-			return avgEnglish;
-		}
-
-		public void setAvgEnglish(int avgEnglish) {
-			this.avgEnglish = avgEnglish;
-		}
-
-		public int getAvgMath() {
-			return avgMath;
-		}
-
-		public void setAvgMath(int avgMath) {
-			this.avgMath = avgMath;
-		}
-
-		public int getAvgWriting() {
-			return avgWriting;
-		}
-
-		public void setAvgWriting(int avgWriting) {
-			this.avgWriting = avgWriting;
-		}
-
-		@Override
-		public String toString() {
-			return "Sat [DBN=" + DBN + ", school=" + school + ", satTakers=" + satTakers + ", avgEnglish=" + avgEnglish
-					+ ", avgMath=" + avgMath + ", avgWriting=" + avgWriting + "]";
-		}
-		
-	}
+	
+	
 
 }
